53b01b3bd525cd1e8e8e906cfec51c37847dcea3
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-11 15:53:48 +0900

    사진도 찍을 수 있는 모양이다.

--- crawler.py
+++ crawler.py
@@ -6,6 +6,9 @@ driver = webdriver.Chrome()
 # 웹 페이지 접근
 driver.get('https://www.naver.com')
 
+# 웹 페이지 스크린샷 저장
+driver.save_screenshot('naver_homepage.png')
+
 # 원하는 데이터 가져오기
 page_title = driver.title
-print(f'Page Title: {page_title}')
+print(f'페이지의 타이틀: {page_title}')
