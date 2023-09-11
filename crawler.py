cd25fb2303ce0939c8eed73afd15ecfb288915fe
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-11 16:23:21 +0900

    네이버 뉴스의 헤드라인을 가져옴

--- crawler.py
+++ crawler.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 # 웹 드라이버 설정
 driver = webdriver.Chrome()
 
 # 웹 페이지 접근
-driver.get('https://www.naver.com')
+driver.get('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100')
 
-# 웹 페이지 스크린샷 저장
-driver.save_screenshot('naver_homepage.png')
+# 헤드라인 뉴스 제목을 가져오기 위한 CSS 선택자
+headlines_selector = 'ul.sh_list li.sh_item div.sh_text a.sh_text_headline'
+
+# CSS 선택자를 이용하여 웹 페이지에서 헤드라인 뉴스를 가져옴
+headlines = driver.find_elements(by=By.CSS_SELECTOR, value=headlines_selector)
+# print(f'수집한 헤드라인 : {headlines}')
+
+# 가져온 헤드라인 뉴스 제목을 출력
+for index, headline in enumerate(headlines):
+    print(f"{index+1}. {headline.text.strip()}")
 
 # 원하는 데이터 가져오기
 page_title = driver.title
